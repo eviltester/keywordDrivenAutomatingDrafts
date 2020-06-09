0c41201d9dd0051e057b2936ff45ab364f22db72
alan <alan@compendiumdev.co.uk>
2020-06-09 10:32:24 +0100

    intial creation ready to start on a test

---
+++ 001-pure-in-memory-keywords/src/test/java/com/eviltester/keyword/initial/CanExecuteKeywords.java
@@ -0,0 +1,13 @@
+package com.eviltester.keyword.initial;
+
+
+import org.junit.jupiter.api.Test;
+
+public class CanExecuteKeywords {
+
+    @Test
+    public void canExecuteKeywords(){
+
+    }
+
+}
