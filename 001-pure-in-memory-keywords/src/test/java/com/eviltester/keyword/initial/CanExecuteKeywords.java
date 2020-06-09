68649758092783dc66d2b22e0701d3a8e8984394
alan <alan@compendiumdev.co.uk>
2020-06-09 10:36:29 +0100

    created script list

--- 001-pure-in-memory-keywords/src/test/java/com/eviltester/keyword/initial/CanExecuteKeywords.java
+++ 001-pure-in-memory-keywords/src/test/java/com/eviltester/keyword/initial/CanExecuteKeywords.java
@@ -3,10 +3,21 @@ package com.eviltester.keyword.initial;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CanExecuteKeywords {
 
     @Test
     public void canExecuteKeywords(){
+
+        List<String> keywordScript = new ArrayList();
+
+        keywordScript.add("open \t https://testpages.herokuapp.com/");
+        keywordScript.add("sleep \t 2000");
+        keywordScript.add("close");
+
+
 
     }
 
